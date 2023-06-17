2db5b330471cd0e70e5b349d9c5a25c480a37fc0
Juan <juan21.naranjo@gmail.com>
2023-06-17 16:04:49 +0200

    Mongoose Population

--- nucampsiteServer/routes/campsiteRouter.js
+++ nucampsiteServer/routes/campsiteRouter.js
@@ -5,8 +5,9 @@ const campsiteRouter = express.Router();
 
 campsiteRouter
   .route("/")
-  .get((req, res) => {
+  .get((req, res, next) => {
     Campsite.find()
+      .populate("comments.author")
       .then((campsites) => {
         res.statusCode = 200;
         res.setHeader("Content-Type", "application/json");
@@ -42,6 +43,7 @@ campsiteRouter
   .route("/:campsiteId")
   .get((req, res, next) => {
     Campsite.findById(req.params.campsiteId)
+      .populate("comments.author")
       .then((campsite) => {
         res.statusCode = 200;
         res.setHeader("Content-Type", "application/json");
@@ -82,6 +84,7 @@ campsiteRouter
   .route("/:campsiteId/comments")
   .get((req, res, next) => {
     Campsite.findById(req.params.campsiteId)
+      .populate("comments.author")
       .then((campsite) => {
         if (campsite) {
           res.statusCode = 200;
@@ -99,6 +102,7 @@ campsiteRouter
     Campsite.findById(req.params.campsiteId)
       .then((campsite) => {
         if (campsite) {
+          req.body.author = req.user._id;
           campsite.comments.push(req.body);
           campsite
             .save()
@@ -148,8 +152,10 @@ campsiteRouter
 
 campsiteRouter
   .route("/:campsiteId/comments/:commentId")
+  .campsiteRouter.route("/:campsiteId/comments/:commentId")
   .get((req, res, next) => {
     Campsite.findById(req.params.campsiteId)
+      .populate("comments.author")
       .then((campsite) => {
         if (campsite && campsite.comments.id(req.params.commentId)) {
           res.statusCode = 200;
